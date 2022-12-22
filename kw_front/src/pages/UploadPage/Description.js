150c7a6638c26ba79c4c72ae1ff626a9c146f63b
sangpyeong <88651495+sangpyeong@users.noreply.github.com>
2022-12-22 18:10:49 +0900

    업로드 구현 전

--- kw_front/src/pages/UploadPage/Description.js
+++ kw_front/src/pages/UploadPage/Description.js
@@ -1,10 +1,10 @@
 function Description() {
   return (
-    <div class="w-full flex-col justify-around">
-      <div class="flex justify-center">
+    <div class="w-full h-60 flex-col justify-around">
+      <div class="h-1/2 flex justify-center">
         <div class="text-center">DWG to TXT Converter</div>
       </div>
-      <div class="flex justify-center">
+      <div class="h-1/2flex justify-center">
         <div class="text-center">
           설명 Convert DWG to TXT online, from any device with a modern browser
           like Chrome and Firefox. Powered by groupdocs.com and groupdocs.cloud.
--- kw_front/src/pages/UploadPage/Upload.js
+++ kw_front/src/pages/UploadPage/Upload.js
@@ -1,6 +1,9 @@
+import image from "./default_img.png";
+
 function Upload() {
   return (
     <div>
+      <img src={image} />
       <div></div>
     </div>
   );
