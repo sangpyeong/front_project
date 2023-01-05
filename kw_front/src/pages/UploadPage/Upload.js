8bd0e7ef3de4eed5d20c78ee7d9f10a828c5839a
sangpyeong <88651495+sangpyeong@users.noreply.github.com>
2023-01-05 13:10:39 +0900

    1:10

--- kw_front/src/pages/UploadPage/Upload.js
+++ kw_front/src/pages/UploadPage/Upload.js
@@ -1,19 +1,10 @@
-import index from "../../index.css";
-import React, {
-  useState,
-  useCallback,
-  useEffect,
-  ChangeEvent,
-  useRef,
-} from "react";
+import { useState } from "react";
 
 function Upload() {
-  const [title, setTitle] = useState("이곳에 파일을 드롭해주세요.");
-  const [dropclass, setDropclass] = useState("dropBox");
-  const [file, setFile] = useState();
-  var $file = document.getElementById("directory_upload");
-  var selectFile = (files) => {
-    $file.files = files;
+  const [fileList, setFileList] = useState();
+  const handleFileInput = (e) => {
+    const file = e.target.files;
+    console.log(file);
   };
   return (
     <div className="flex flex-col justify-center w-full">
@@ -24,45 +15,24 @@ function Upload() {
           encType="multipart/form-data"
           className="flex justify-center flex-col items-center"
         >
-          <div
-            class={dropclass}
-            onDrop={(e) => {
-              e.preventDefault();
-              const files = [...e.dataTransfer?.files];
-              console.log(files);
-              setTitle(files.map((v) => v.name).join("<br>"));
-              setDropclass("dropBox");
-              setFile(files);
-              selectFile(files);
-            }}
-            onDragOver={(e) => {
-              e.preventDefault();
-              setDropclass("dropBoxactive");
-            }}
-            onDragEnter={(e) => {
-              e.preventDefault();
-              setDropclass("dropBoxactive");
-            }}
-            onDragLeave={(e) => {
-              e.preventDefault();
-              setDropclass("dropBox");
-            }}
-          >
-            <h1>{title}</h1>
+          <div className="flex flex-row w-full justify-center h-[30px]">
+            <input
+              type="file"
+              id="directory_upload"
+              className="flex w-1/2 h-full"
+              multiple
+              webkitdirectory="true"
+              onChange={handleFileInput}
+              accept=".pdf"
+            />
+            <button
+              type="submit"
+              className="flex w-1/6 justify-center border border-slate-400 bg-slate-100 items-center h-full"
+              onClick={() => {}}
+            >
+              업로드
+            </button>
           </div>
-          <input
-            type="file"
-            id="directory_upload"
-            multiple
-            webkitdirectory="true"
-            onChange={(e) => {
-              console.log(e.target.files);
-              setFile(e.target.files);
-              setTitle(e.target.files[0].name);
-            }}
-          />
-          <div id="preview"></div>
-          <input type="submit" />
         </form>
       </div>
     </div>
